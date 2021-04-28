9f9d96d6f765659c5d88e50ead5e4d79e039211b
PashkevichTania <pashkevichtania2004@gmail.com>
2021-04-28 12:54:17 +0300

    finish task

---
+++ WinButNum/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WinButNum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickButton1 = new WinButNum.ClickButton();
+            this.SuspendLayout();
+            // 
+            // clickButton1
+            // 
+            this.clickButton1.Location = new System.Drawing.Point(261, 276);
+            this.clickButton1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.clickButton1.Name = "clickButton1";
+            this.clickButton1.Size = new System.Drawing.Size(150, 45);
+            this.clickButton1.TabIndex = 0;
+            this.clickButton1.Text = "clickButton1";
+            this.clickButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 554);
+            this.Controls.Add(this.clickButton1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ClickButton clickButton1;
+    }
+}
+
